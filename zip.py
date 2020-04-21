c02c5dff4838c87c9d6b65cc6793c9aa8a546d17
Yarik <yarik@MacBook-Pro-Yarik.local>
2020-04-21 13:48:06 +0300

    Unnecessary files deleted

--- zip.py
+++
@@ -1,7 +0,0 @@
-import zipfile #подключаем модуль
-zname=r'cert.rar'
-name = 'skypework1234@gmail.com'
-newzip = zipfile.ZipFile(zname, 'w')
-newzip.write('{}.pfx'.format(name))
-newzip.setpassword(b'1')
-newzip.close() #закрываем архив
